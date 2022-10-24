0bb23cff8161324cc7cd9e996ef4451c0817c4dc
Taras Sirenko <tarassirenko71@gmail.com>
2022-10-24 23:39:40 +0300

    первую задачу закончил, заливаю чтобы протестить сборку

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+let setIntervalId = null;
+let startBtnActiv = false;
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(event) {
+  if (startBtnActiv) return;
+  startBtnActiv = true;
+  event.currentTarget.setAttribute('disabled', 'true');
+  refs.stopBtn.removeAttribute('disabled');
+
+  setIntervalId = setInterval(() => {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+}
+function onStopBtnClick(event) {
+  refs.startBtn.removeAttribute('disabled');
+  event.currentTarget.setAttribute('disabled', 'true');
+  clearInterval(setIntervalId);
+  startBtnActiv = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,8 @@
+const refs = {
+  dateInput: document.querySelector('#datetime-picker'),
+};
+
+const currentDate = new Date();
+console.log(currentDate);
+
+// refs.dateInput.value = currentDate;
