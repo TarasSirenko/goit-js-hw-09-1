8cbeb57f71326c8ee800fa2b8fcbea01d038455b
Taras Sirenko <tarassirenko71@gmail.com>
2022-10-26 22:15:03 +0300

    заканчивает новую домашку, и 1,2 задание в старой добавляет в комент 3 задачи

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,8 +1,82 @@
+import flatpickr from 'flatpickr';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'flatpickr/dist/flatpickr.min.css';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
 const refs = {
   dateInput: document.querySelector('#datetime-picker'),
+  timerStartBtn: document.querySelector('[data-btn="start"]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
 };
 
-const currentDate = new Date();
-console.log(currentDate);
+let timerStopBtnRef = {};
+let userSlectedTime = 0;
+let setIntervalId = null;
 
-// refs.dateInput.value = currentDate;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() - Date.now() > 0) {
+      refs.timerStartBtn.removeAttribute('disabled');
+      userSlectedTime = selectedDates[0].getTime();
+    } else {
+      refs.timerStartBtn.setAttribute('disabled', 'true');
+      Notify.failure('Please choose a date in the future');
+    }
+  },
+};
+
+flatpickr(refs.dateInput, options);
+
+refs.timerStartBtn.addEventListener('click', onStartTimerBtnClick);
+
+function onStartTimerBtnClick(event) {
+  event.target.textContent = 'Stop';
+  event.target.dataset.btn = 'stop';
+  timerStopBtnRef = document.querySelector('[data-btn="stop"]');
+  timerStopBtnRef.addEventListener('click', onStopTimerBtnClick);
+  Notify.success('Timer started!');
+  setIntervalId = setInterval(() => {
+    const timeLeftMs = userSlectedTime - Date.now();
+    if (timeLeftMs < 0) {
+      onStopTimerBtnClick(event);
+      return;
+    }
+    const formattedRemainingTime = convertMs(timeLeftMs);
+    addLeadingZero(formattedRemainingTime);
+  }, 1000);
+  refs.timerStartBtn.removeEventListener('click', onStartTimerBtnClick);
+}
+function onStopTimerBtnClick(event) {
+  refs.timerStartBtn.addEventListener('click', onStartTimerBtnClick);
+  Notify.success('Timer stopped!');
+  event.target.textContent = 'Start';
+  event.target.dataset.btn = 'start';
+  clearInterval(setIntervalId);
+  timerStopBtnRef.removeEventListener('click', onStopTimerBtnClick);
+}
+
+function addLeadingZero({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${String(days).padStart(2, '0')}`;
+  refs.hours.textContent = `${String(hours).padStart(2, '0')}`;
+  refs.minutes.textContent = `${String(minutes).padStart(2, '0')}`;
+  refs.seconds.textContent = `${String(seconds).padStart(2, '0')}`;
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,122 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+// const refs = {
+//   firstDelayInput: document.querySelector('[name="delay"]'),
+//   stepDelayInput: document.querySelector('[name="step"]'),
+//   amountPromises: document.querySelector('[name="amount"]'),
+//   form: document.querySelector('.form'),
+// };
+
+// refs.form.addEventListener('submit', OnSubmitForm);
+
+// function OnSubmitForm(event) {
+//   event.preventDefault();
+//   const firstDelay = parseInt(event.srcElement[0].value);
+//   const stepDelay = parseInt(event.srcElement[1].value);
+//   const amountPromises = parseInt(event.srcElement[2].value);
+//   renderPromisesOnSubmitForm(firstDelay, stepDelay, amountPromises);
+// }
+
+// function renderPromisesOnSubmitForm(delay, stepDelay, amountPromises) {
+//   let position = 0;
+//   for (let i = 0; i < amountPromises; i++) {
+//     delay += stepDelay;
+//     position += 1;
+//     createPromise(position, delay)
+//       .then(({ position, delay }) => {
+//         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       })
+//       .catch(({ position, delay }) => {
+//         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//       });
+//   }
+// }
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       }
+//       reject({ position, delay });
+//     }, delay);
+//   });
+// }
+
+//  Задачи со старой программы ---------------------
+//  задача 1
+
+// const delay = ms => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(ms);
+//     }, ms);
+//   });
+// };
+
+// const logger = time => Notify.success(`Resolved after ${time}ms`);
+
+// delay(2000).then(logger);
+// delay(1000).then(logger);
+// delay(1500).then(logger);
+
+//  Задача 2 -----------------
+// const users = [
+//   { name: 'Mango', active: true },
+//   { name: 'Poly', active: false },
+//   { name: 'Ajax', active: true },
+//   { name: 'Lux', active: false },
+// ];
+
+// const toggleUserState = (allUsers, userName) => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user
+//   );
+//   return new Promise((resolve, reject) => {
+//     resolve(updatedUsers);
+//     reject(updatedUsers);
+//   });
+// };
+// const logger = updatedUsers => console.table(updatedUsers);
+
+// toggleUserState(users, 'Mango').then(logger);
+// toggleUserState(users, 'Lux').then(logger);
+
+// Задача 3
+
+// const randomIntegerFromInterval = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// };
+
+// const makeTransaction = ({ id }) => {
+//   const delay = randomIntegerFromInterval(200, 500);
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const canProcess = Math.random() > 0.3;
+
+//       if (canProcess) {
+//         resolve({ id, delay });
+//       } else {
+//         reject(id);
+//       }
+//     }, delay);
+//   });
+// };
+
+// const logSuccess = ({ id, delay }) => {
+//   console.log(`Transaction ${id} processed in ${delay}ms`);
+// };
+
+// const logError = id => {
+//   console.warn(`Error processing transaction ${id}. Please try again later.`);
+// };
+
+// makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+
+// makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+
+// makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+
+// makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
